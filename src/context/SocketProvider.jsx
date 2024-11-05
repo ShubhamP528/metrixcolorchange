c10a49162ded9c5eb16ca0fff49917a031ba2550
Shubham Prajapati <shubham528prajapati@gmail.com>
2024-11-05 06:50:33 +0530

    changes done

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,10 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8080"), []);
+  const socket = useMemo(
+    () => io("https://videocalling-backend.onrender.com"),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>
